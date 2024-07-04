07bb5273f61b35440c8eb37975a0359ed3ced5e6
iQ <estrizqy@gmail.com>
2024-07-04 22:11:31 +0700

    feat: fetching product data

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -7,19 +7,25 @@ import {
     CardHeader,
     CardTitle
 } from '@/components/ui/card'
+
+defineProps({
+    product: {
+        type: Object,
+    }
+})
 </script>
 <template>
     <div class="w-[330px]">
         <Card>
             <CardHeader>
-                <CardTitle>Title</CardTitle>
+                <CardTitle>{{ product.name }}</CardTitle>
                 <CardDescription
-                    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio eius dolores
-                    officiis eos tenetur id.</CardDescription
+                    ><p>{{ product.varian }}</p></CardDescription
                 >
             </CardHeader>
-            <CardContent>Content</CardContent>
+            <CardContent><img class="w-fit" :src="product.photo" alt="Product Photo"></CardContent>
             <CardFooter>
+                <p class="text-xl font-bold pr-2">Rp. {{ product.price }}</p>
                 <button class="py-2 px-5 bg-orange-500 rounded">Buy Now</button>
             </CardFooter>
         </Card>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -10,18 +10,33 @@ import {
     DialogTrigger
 } from '@/components/ui/dialog'
 import ProductCard from '@/components/ProductCard.vue';
+import { ref } from 'vue';
+import axios from 'axios';
+
+const loading = ref(false);
+
+const products = ref([])
+
+const fetchProducts = async () => {
+    // loading.value = true
+    try {
+        const response = await axios.get('https://sistemtoko.com/public/demo/product')
+        const data = products.value = response.data.aaData
+        console.log("Data: ", data)
+    } catch (error) {
+        console.log("Error Fetching: ", error)
+    } finally {
+        loading.value = false
+    }
+}
+fetchProducts()
 </script>
 
 <template>
     <Navbar />
     <div class="px-5 py-8">
         <div class="grid grid-cols-3 place-items-center gap-4">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+            <ProductCard v-for="product in products" :key="product.id" :product="product"></ProductCard>
         </div>
     </div>
 </template>
